a668f3c287874c749f6912270a0c55e8ec1efc48
dakotacookenmaster <dakotacookenmaster@southern.edu>
2020-02-25 12:20:41 -0500

    Added to view inventory.

--- shine/vending.py
+++ shine/vending.py
@@ -8,6 +8,7 @@
 
 class VendingMachine():
     def __init__(self):
+        ''' Constructs a VendingMachine object and initializes the inventory to a default 10 x 10. '''
         self.inventory = {}
         self.row_size = 10
         self.col_size = 10
@@ -19,30 +20,38 @@ class VendingMachine():
                 self.inventory[first_letter + str(col)] = []
 
     def purchase_item(self, id):
-        '''purchase_item(id) --> bool;
-        When passed an id, purchase_item will check if there are items in that id to purchase
+        '''
+            purchase_item(id) --> bool
+            When passed an id, purchase_item will check if there are items in that id to purchase
         '''
         keys = self.inventory.keys()
 
         if id in keys:
-            
             if id != []:
                 print("This item costs: " + id[2])
                 money = input("Please pay here: ")
                 if money == id[2]:
-
+                    pass
         else:
             print("Invalid Choice")
             return False
-        
-        pass
 
     def view_inventory(self):
-        pass
+        # Handle dynamic title width
+        string_max = 0
+        for item in self.inventory.values():
+            local_max = if x := list(map(len, item)).empty()
+            if local_max > string_max:
+                string_max = local_max
+
+        print("=" * (max // 2))
+        for item in self.inventory:
+            print(self.inventory[item])
 
     def prompt(self):
         pass
 
 myVen = VendingMachine() 
-print(myVen.inventory)
+myVen.inventory["A0"] = ["a", "abcdadfefaefads", "ajfoejaojfoaj9ejaojfoaadjl"]
+myVen.view_inventory()
 # {id: [name, quantity, price]}
