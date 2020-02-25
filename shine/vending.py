0cd8e3c9e76bacf5c4cabfcea64ff002b3a4cada
dakotacookenmaster <dakotacookenmaster@southern.edu>
2020-02-25 11:04:40 -0500

    Added vending.py file.

---
+++ shine/vending.py
@@ -0,0 +1,13 @@
+# LEAP 0: *Shine*
+# CPTR-405-A: Organization of Programming Languages
+# filename: vending.py
+# Programmed by:
+#   Dakota Cookenmaster
+#   Michael Yoon
+# Last edit: 02/25/2020
+
+class VendingMachine():
+    def __init__(self):
+        pass
+
+
